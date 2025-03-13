c12eba1425747e61db941070d5b6ca205b430f58
Helena Hollænder <helena.hollaender@gmail.com>
2025-03-13 17:06:37 +0100

    layout_toys

--- src/views/projects/ToysView.vue
+++ src/views/projects/ToysView.vue
@@ -1,10 +1,15 @@
 <template>
     <div class="grid">
         <img src="/images/toys/toysSketch01.png" class="w-[70vw] justify-self-end pt-4">
-        <img src="/images/toys/toysSketch02.png" class="w-[70vw] justify-self-start translate-y-[-20px]">
+        <img src="/images/toys/toysSketch02.png" class="w-[70vw] justify-self-start translate-y-[-20px] translate-x-[-20px]">
         <div class="h-[300px] content-end">
             <video autoplay muted loop playsinline class="w-[50vw] justify-self-center">
                 <source src="/videos/toys01.mp4" type="video/mp4">
+            </video>
+        </div>
+        <div class="h-[260px] content-end">
+            <video autoplay muted loop playsinline class="w-[46vw] justify-self-start translate-x-[-18px]">
+                <source src="/videos/demoFloorToys.mp4" type="video/mp4">
             </video>
         </div>
     </div>
@@ -17,6 +22,9 @@
         </div>
         <div class="h-[338px] content-end">
     <img src="/images/toys/toyScribbleD01.png" class="w-[32vw] justify-self-end mr-4">
+        </div>
+        <div class="h-[278px] content-end">
+    <img src="/images/toys/scribbleDemoFloor.png" class="w-[10vw] justify-self-start ml-[40vw]">
         </div>
     </div>
 </template>
