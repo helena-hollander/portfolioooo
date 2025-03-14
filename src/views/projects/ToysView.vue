2662662448e193ac9c442eb37e3e08dc66206723
Helena Hollænder <helena.hollaender@gmail.com>
2025-03-14 16:19:17 +0100

    mt-minus-test

--- src/views/projects/ToysView.vue
+++ src/views/projects/ToysView.vue
@@ -13,10 +13,16 @@
         <img src="/images/toys/toysFloggerBlender01.png" class="w-[36%] mt-[75%] justify-self-center ">
         <img src="/images/toys/toysGeneratorProcessing01.png" class="w-full mt-[10%] justify-self-center z-10">
         <img src="/images/toys/toysFloggerPixel01.png" class="w-[90%] justify-self-end translate-y-[-12%] translate-x-[40px]">
-        <img src="/images/toys/toysProcessing02.png" class="w-[38%] justify-self-end translate-y-[-5%]">
-        <video autoplay muted loop playsinline class="w-[54%] justify-self-start translate-y-[-22%]">
+        <img src="/images/toys/toysProcessing02.png" class="w-[38%] justify-self-end mt-[-10%]">
+        <video autoplay muted loop playsinline class="w-[54%] justify-self-start mt-[-18%]">
                 <source src="/videos/toysTweaks.mp4" type="video/mp4">
          </video>
+    <div class="grid grid-cols-3 mt-[4%] gap-[8%]">
+        <img src="/images/toys/toysSatisfyerPoster.png" class="w-full">
+        <img src="/images/toys/toysDildPoster.png" class="w-full">
+        <img src="/images/toys/toysWandPoster.png" class="w-full">
+    </div>
+    <img src="/images/toys/toysCollection3d01.png" class="w-[96%] mt-[20%] justify-self-center">
     </div>
 
     <!-- scribble div -->
@@ -25,10 +31,12 @@
      <img src="/images/toys/toysScribbleHearts01.png" class="w-[30%] mt-[16%] justify-self-end mr-[10%]">
      <img src="/images/toys/toyScribbleD01.png" class="w-[32%] mt-[106%] justify-self-end mr-[4%]">
      <img src="/images/toys/scribbleDemoFloor.png" class="w-[10%] mt-[24%] justify-self-start ml-[41%]">
-     <img src="/images/toys/toysScribbleGenerator01.png" class="w-[54%] mt-[129%] justify-self-center">
+     <img src="/images/toys/toysScribbleGenerator01.png" class="w-[58%] mt-[129%] justify-self-center">
      <img src="/images/toys/toysSvgScribble01.png" class="w-[30%] mt-[96%] justify-self-start ml-[12%]">
-     <img src="/images/toys/toysTweaksScribble01.png" class="w-[22%] mt-[58%] justify-self-start ml-[32%]">
-     <img src="/images/toys/toysCollectionScribble01.png" class="w-[52%] mt-[88%] justify-self-center">
+     <img src="/images/toys/toysTweaksScribble01.png" class="w-[22%] mt-[48%] justify-self-start ml-[32%]">
+    
+     <img src="/images/toys/toysCollectionScribble01.png" class="w-[52%] mt-[138%] justify-self-center">
+   
         <!-- <div class="h-[0px] content-end">
     <img src="/images/toys/scribbleDemoFloor.png" class="w-[10vw] justify-self-start ml-[40vw]">
         </div> -->
