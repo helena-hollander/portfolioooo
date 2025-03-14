711144d78b2287c0c3040f30596f76b1c3484d33
Helena Hollænder <helena.hollaender@gmail.com>
2025-03-14 11:39:52 +0100

    freja_responsitvitet_fix_mt_procent

--- src/views/projects/ToysView.vue
+++ src/views/projects/ToysView.vue
@@ -2,31 +2,25 @@
 
 <template>
     <div class="grid">
-        <img src="/images/toys/toysSketch01.png" class="w-[70vw] justify-self-end pt-4">
-        <img src="/images/toys/toysSketch02.png" class="w-[70vw] justify-self-start translate-y-[-20px] translate-x-[-20px]">
-        <div class="h-[300px] content-end">
-            <video autoplay muted loop playsinline class="w-[50vw] justify-self-center">
+        <img src="/images/toys/toysSketch01.png" class="w-[70%] justify-self-end pt-4">
+        <img src="/images/toys/toysSketch02.png" class="w-[70%] justify-self-start translate-y-[-20px] translate-x-[-20px]">
+        <video autoplay muted loop playsinline class="w-[50%] justify-self-center mt-[10%]">
                 <source src="/videos/toys01.mp4" type="video/mp4">
-            </video>
-        </div>
-        <div class="h-[260px] content-end">
-            <video autoplay muted loop playsinline class="w-[46vw] justify-self-start translate-x-[-18px]">
+         </video>
+         <video autoplay muted loop playsinline class="w-[46%] mt-[10%] justify-self-start translate-x-[-18px]">
                 <source src="/videos/demoFloorToys.mp4" type="video/mp4">
-            </video>
-        </div>
-        <div class="h-[500px] content-end">
-        <img src="/images/toys/toysFloggerBlender01.png" class="w-[36vw] justify-self-center ">
-        </div>
-        <img src="/images/toys/toysGeneratorProcessing01.png" class="w-full justify-self-center mt-12 z-10">
-        <img src="/images/toys/toysFloggerPixel01.png" class="w-[90vw] justify-self-end translate-y-[-58px] translate-x-[40px]">
-        <img src="/images/toys/toysProcessing02.png" class="w-[38vw] justify-self-end translate-y-[-48px]">
-        <video autoplay muted loop playsinline class="w-[54vw] justify-self-start translate-y-[-98px]">
+        </video>
+        <img src="/images/toys/toysFloggerBlender01.png" class="w-[36%] mt-[75%] justify-self-center ">
+        <img src="/images/toys/toysGeneratorProcessing01.png" class="w-full mt-[10%] justify-self-center z-10">
+        <img src="/images/toys/toysFloggerPixel01.png" class="w-[90%] justify-self-end translate-y-[-12%] translate-x-[40px]">
+        <img src="/images/toys/toysProcessing02.png" class="w-[38%] justify-self-end translate-y-[-5%]">
+        <video autoplay muted loop playsinline class="w-[54%] justify-self-start translate-y-[-7%]">
                 <source src="/videos/toysTweaks.mp4" type="video/mp4">
          </video>
     </div>
 
     <!-- scribble div -->
-    <div class="absolute top-0 grid w-full">
+    <!-- <div class="absolute top-0 grid w-full">
         <div class="h-[228px] content-end">
         <img src="/images/toys/toyScribble01.png" class="w-[50vw] ml-4">
         </div>
@@ -38,9 +32,9 @@
         </div>
         <div class="h-[278px] content-end">
     <img src="/images/toys/scribbleDemoFloor.png" class="w-[10vw] justify-self-start ml-[40vw]">
-        </div>
+        </div> -->
         <!-- <div class="h-[0px] content-end">
     <img src="/images/toys/scribbleDemoFloor.png" class="w-[10vw] justify-self-start ml-[40vw]">
         </div> -->
-    </div>
+    <!-- </div> -->
 </template>
