3036414958c02e29772f958f113f6a8f95756a0d
Helena Hollænder <helena.hollaender@gmail.com>
2025-03-15 16:39:37 +0100

    start_repsonsivt_toys

--- src/views/projects/ToysView.vue
+++ src/views/projects/ToysView.vue
@@ -4,10 +4,10 @@ import FooterCom from '@/components/universal/FooterCom.vue';
 </script>
 
 <template>
-    <div class="grid">
-        <img src="/images/toys/toysSketch01.png" class="w-[70%] justify-self-end pt-4">
-        <img src="/images/toys/toysSketch02.png" class="w-[70%] justify-self-start translate-y-[-20px] translate-x-[-20px]">
-        <video autoplay muted loop playsinline class="w-[50%] justify-self-center mt-[10%]">
+    <div class="grid sm:grid-cols-2">
+        <img src="/images/toys/toysSketch01.png" class="w-[70%] justify-self-end pt-4 sm:w-[86%] sm:cols-start-2">
+        <img src="/images/toys/toysSketch02.png" class="w-[70%] justify-self-start translate-y-[-20px] translate-x-[-20px] sm:w-[86%] sm:mt-[-28%] sm:col-start-1">
+        <video autoplay muted loop playsinline class="w-[50%] justify-self-center mt-[10%] sm:w-[46%] sm:col-start-1 sm:row-start-1 sm:justify-self-end sm:mt-0"> ">
             <source src="/videos/toys01.mp4" type="video/mp4">
         </video>
         <div class="grid grid-cols-2 w-full mt-[10%] text-right justify-self-start font-trispace text-darkest text-contextSizeMobile/[1.8]">
@@ -41,11 +41,11 @@ import FooterCom from '@/components/universal/FooterCom.vue';
     </div>
 
     <!-- scribble div -->
-    <div class="absolute top-0 grid w-full">   
-     <img src="/images/toys/toyScribble01.png" class="w-[50%] mt-[30%] ml-[4%]">
-     <img src="/images/toys/toysScribbleHearts01.png" class="w-[28%] mt-[44%] justify-self-end mr-[10%]">
-     <img src="/images/toys/toyScribbleD01.png" class="w-[32%] mt-[80%] justify-self-end mr-[4%]">
-     <img src="/images/toys/toysLogoBlue.png" class="w-[16%] mt-[0%] justify-self-start ml-[26%]">
+    <div class="absolute top-0 grid w-full sm:grid-cols-3">   
+     <img src="/images/toys/toyScribble01.png" class="w-[50%] mt-[30%] ml-[4%] sm:col-start-1 sm:w-[76%] sm:mt-[44%] sm:ml-[24%]">
+     <img src="/images/toys/toysScribbleHearts01.png" class="w-[28%] mt-[44%] justify-self-end mr-[10%] sm:col-start-3 sm:row-start-2 sm:w-[34%] sm:justify-self-start">
+     <img src="/images/toys/toyScribbleD01.png" class="w-[32%] mt-[80%] justify-self-end mr-[4%] sm:col-start-2 sm:row-start-1 sm:w-[44%] sm:mr-[20%] sm:mt-[34%]">
+     <img src="/images/toys/toysLogoBlue.png" class="w-[16%] mt-[0%] justify-self-start ml-[26%] sm:col-start-1 sm:w-[34%] sm:justify-self-end sm:mt-[80%] sm:mr-[-38%]">
      <img src="/images/toys/scribbleDemoFloor.png" class="w-[10%] mt-[13%] justify-self-start ml-[41%]">
     
    
@@ -54,13 +54,11 @@ import FooterCom from '@/components/universal/FooterCom.vue';
         </div> -->
     </div>
     <!-- displayText div -->
-    <div class="absolute top-0 grid w-full">
-        <h1 class="mt-[124%] mr-[4%] font-trispace text-darkest text-displaySizeMobile/[1.15] text-right">the<br>SEXTOY<br>GALLERY</h1>
-        <p class="mr-[4%] font-trispace text-darkest text-right text-contextSizeMobile">[school project, June '24]</p>
-        <!-- <div class="grid grid-cols-2 justify-content-end content-end mr-[4%] gap-[10%] h-[136vw]">
-            <p class="col-start-2 self-end h-full font-trispace text-darkest text-right text-contextSizeMobile/[1.8]">Visual identity combining 3d models with Processing filters, to create consistent and generative logo-assets.</p>
-        </div> -->
+    <!-- <div class="absolute top-0 grid w-full sm:grid-cols-2 justify-items-end">
+        <h1 class="mt-[124%] mr-[4%] font-trispace text-darkest text-displaySizeMobile/[1.15] text-right w-[36%] text-wrap sm:col-start-2 sm:w-[90%] sm:text-5xl">the SEXTOY GALLERY</h1>
+        <p class="mr-[4%] font-trispace text-darkest text-right text-contextSizeMobile text-nowrap">[school project, June '24]</p>
+      
 
-    </div>
+    </div> -->
     <FooterCom />
 </template>
