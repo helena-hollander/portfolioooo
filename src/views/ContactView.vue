c426ca590daaf7458a32568804f182cb1007d529
Helena Hollænder <helena.hollaender@gmail.com>
2025-03-17 12:33:03 +0100

    internship_benner_span

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -2,14 +2,18 @@
 
 </script>
 <template>
-    <div class="grid h-screen bg-lightContrast pt-[10%] content-start">
+    <div class="grid  h-full bg-lightContrast pt-[10%] content-start sm:h-screen">
         <h2 class="mt-[10%] ml-[4%] font-trispace text-3xl text-darkest w-[360px]">Let’s share ideas and get creative.</h2>
         <img src="/images/scribbles/contact/scribbleBubbleSmall.png" class="w-[100%] mt-[-28%]">
         <!-- <img src="/images/scribbles/contact/scribbleShare.png" class="w-[12%] mt-[-23%] ml-[11%]"> -->
 
     
-        <div class="grid grid-cols-3">
-
+        <div class="grid font-trispace text-darkest text-lg mt-[-10%]">
+            <p class="m-[4%]">I am currently looking for an 
+                
+                <img src="/images/scribbles/contact/scribbleCircle.png" class="w-[152px] inline relative"><span class="ml-[-130px]">internship</span></img> 
+                
+                as part of my bachelor in Coded Design, DMJX, Copenhagen. The period is from 20th of October ‘25 - 6th of March ‘26. (It can be split into two halves) <br><br>I would love to explore the crossover between interactive design, creative code, 3d and installation/stage design. I want to tell stories and facilitate rooms where people dare to dance and dream. <br><br>Other work inquiries are also very welcome. Big or small.</p>
         </div>
     </div>
    
