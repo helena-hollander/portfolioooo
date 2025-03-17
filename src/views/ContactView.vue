a85df110a49d75e85db148936b68bd9a6d8f3498
Helena Hollænder <helena.hollaender@gmail.com>
2025-03-17 11:36:01 +0100

    frokost_igen_mandag

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -2,9 +2,9 @@
 
 </script>
 <template>
-    <div class="grid h-screen bg-lightContrast pt-[10%]">
-        <img src="/images/scribbles/contact/scribbleBubbleSmall.png" class="w-[100%]">
-        <h2 class="mt-[-64%] ml-[4%] font-trispace text-3xl text-darkest">Let’s share ideas and get creative.</h2>
+    <div class="grid h-screen bg-lightContrast pt-[10%] content-start">
+        <h2 class="mt-[10%] ml-[4%] font-trispace text-3xl text-darkest w-[360px]">Let’s share ideas and get creative.</h2>
+        <img src="/images/scribbles/contact/scribbleBubbleSmall.png" class="w-[100%] mt-[-28%]">
         <!-- <img src="/images/scribbles/contact/scribbleShare.png" class="w-[12%] mt-[-23%] ml-[11%]"> -->
 
     
