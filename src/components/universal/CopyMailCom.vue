d925688c5b821f2b271e75f15cb457582baccd26
Helena Hollænder <helena.hollaender@gmail.com>
2025-03-18 10:46:57 +0100

    layout_contact_small

---
+++ src/components/universal/CopyMailCom.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+
+const copyToClipboard = () => {
+  // Create a textarea element to hold the text
+  const textarea = document.createElement('textarea');
+  textarea.value = 'helena.hollaender@gmail.com'; // Replace with your text
+
+  // Make the textarea hidden
+  textarea.style.position = 'fixed';
+  textarea.style.left = '-9999px';
+
+  // Append the textarea to the document
+  document.body.appendChild(textarea);
+
+  // Select and copy the text
+  textarea.select();
+  document.execCommand('copy');
+
+  // Clean up - remove the textarea
+  document.body.removeChild(textarea);
+
+  // Optionally, provide feedback to the user
+  alert('E-mail copied to clipboard: ' + textarea.value);
+}
+</script>
+
+<template>
+  <button @click="copyToClipboard">
+    <img src="/images/scribbles/contact/scribbleMail.png" alt="Copy Email">
+  </button>
+</template>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,5 +1,5 @@
 <script setup>
-
+import CopyMailCom from "@/components/universal/CopyMailCom.vue";
 </script>
 <template>
     <div class="grid  h-full bg-lightContrast pt-[10%] content-start sm:h-screen">
@@ -15,9 +15,12 @@
                 as part of my bachelor in Coded Design, DMJX, Copenhagen. The period is from 20th of October ‘25 - 6th of March ‘26. (It can be split into two halves) <br><br>I would love to explore the crossover between interactive design, creative code, 3d and installation/stage design. I want to tell stories and facilitate rooms where people dare to dance and dream. <br><br>Other work inquiries are also very welcome. Big or small.<img src="/images/scribbles/contact/pTagEnd.png" class="w-[70px] inline"></img></p>
         </div>
         <div class="grid">
-            <a href="https://www.behance.net/helenahollnder" target="_blank"><img src="/images/scribbles/contact/scribbleBehance.png" class="w-[64%] justify-self-end hover:opacity-60"></a>
+            <img src="/images/scribbles/contact/scribbleCursor.png" class="w-[20%] mt-[-10%] justify-self-end">
+            <a href="https://www.behance.net/helenahollnder" target="_blank"><img src="/images/scribbles/contact/scribbleBehance.png" class="w-[64%] justify-self-end hover:opacity-60 mt-[-12%]"></a>
             <a href="https://www.linkedin.com/in/helena-holl%C3%A6nder-b0641728b/" target="_blank"><img src="/images/scribbles/contact/scribbleLinkedin.png" class="w-[70%] justify-self-end hover:opacity-60"></a>
             <a href="https://www.instagram.com/hollanderen/" target="_blank"><img src="/images/scribbles/contact/scribbleInsta.png" class="w-[80%] justify-self-end hover:opacity-60"></a>
+            <img src="/images/scribbles/contact/scribbleClickMail.png" class="w-[38%] ml-[-8%] mt-[-12%]">
+            <CopyMailCom class="mt-[-2%]"></CopyMailCom>
         </div>
     </div>
    
