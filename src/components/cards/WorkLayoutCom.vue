f4a57e9294931cbcca1a7b8bd6565fc69529fa82
Helena Hollænder <helena.hollaender@gmail.com>
2025-04-16 14:41:34 +0200

    video_card_work

--- src/components/cards/WorkLayoutCom.vue
+++ src/components/cards/WorkLayoutCom.vue
@@ -1,12 +1,15 @@
 <script setup>
 import WorkCardCom from '@/components/cards/WorkCardCom.vue';
+import WorkVideoCardCom from '@/components/cards/WorkVideoCardCom.vue';
 </script>
 
 <template>
-<div class="grid grid-cols-1 gap-0 mt-[28%] sm:mt-0 sm:grid-cols-10 sm:grid-rows-7 sm:gap-[0%] sm:pt-[4%] mx-[8%] text-darkest font-bold">
+<div class="grid grid-cols-1 gap-0 mt-[28%] sm:mt-0 sm:grid-cols-10  sm:gap-[0%] sm:pt-[4%] mx-[8%] text-darkest font-bold">
     
-    <WorkCardCom 
-      imageUrl="/images/toys/toysWandPoster.png"
+    <WorkVideoCardCom 
+      videoUrl="/videos/toys01.mp4"
+      widthVideo="300px"
+      heightVideo="400px"
       title="SEXTOY GALLERY"
       description="Optional description text here"
       class="sm:col-span-2 sm:row-start-1 sm:row-span-1 col-span-1 row-span-3" />
@@ -15,8 +18,8 @@ import WorkCardCom from '@/components/cards/WorkCardCom.vue';
       title="STUDIEKORT"
       class="sm:col-span-4 sm:col-start-5 col-span-1 col-start-1"/>
     <WorkCardCom 
-      imageUrl="/images/work_tags/studiekort_card.png"
-      title="STUDIEKORT"
+      imageUrl="/images/toys/toysWandPoster.png"
+      title="TESTER"
       class="sm:col-span-4 sm:col-start-3 col-span-1 col-start-1"/>
       
   </div>
---
+++ src/components/cards/WorkVideoCardCom.vue
@@ -0,0 +1,38 @@
+<script setup>
+// Define props with their types and default values
+defineProps({
+  videoUrl: {
+    type: String,
+    required: true
+  },
+  widthVideo: {
+    type: String,
+    required: true
+  },
+  heightVideo: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    default: ""
+  }
+});
+</script>
+
+<template>
+  <div class="grid grid-rows-2 mb-[2%] sm:grid-rows-2">
+    <video autoplay muted loop playsinline :width="widthVideo" :height="heightVideo" :alt="title" class="row-span-2 sm:row-span-1 sm:h-[100%]">
+      <source :src="videoUrl" type="video/mp4">
+      Your browser does not support the video tag.
+    </video>
+    <div class="row-span-1">
+    <h1 class="text-xl mt-2">{{ title }}</h1>
+    <p v-if="description" class="font-extralight">{{ description }}</p>
+  </div>
+  </div>
+</template>
