59d646893cd9f8575adf5d74ee1483b9173fbf7b
Helena Hollænder <helena.hollaender@gmail.com>
2025-04-16 22:42:06 +0200

    about_start_page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,30 @@
 <template>
  <div class="h-[100vh] bg-lightest">
+    <div class="grid grid-cols-7">
+        <img src="/images/about/tulips_portrait.png" class="col-span-3 h-[100vh] object-cover" />
+        <p class="col-span-4 mt-[12%] mr-[4%] font-vazirmatn text-lg/6 font-light ml-[-6%]">
+Hi,
 
+<br><br>My name is____________ (she/her) and I am a visual creator, illustrator, explorer, coder and artist.
+<br><br>I am inspired by  ___________ .
+<br><br>I am currently studying for my BA in Visual Communication, ____________ at DMJX, Copenhagen. I am looking for an internship from 20th of October ‘25 to 6th of March ‘26. 
+
+<br><br>I specialize in interactive design and stage design through creative coding, 3D and motion. Anything within the realm of theatre, museum installations, websites, dancefloors, graphic novels and game design. 
+
+<br><br>My process starts __________, always. Drawing is thinking, and the best way for me to map out ideas with friends and colleagues. 
+
+<br><br>I dream about ___________ .
+
+<br><br>In the future I will ____________(create spaces where people meet) Themselves and each other. I want to design a feeling of being connected and present despite our differences. Dare people to dance, whatever that may look like. 
+
+<br><br>I am a queer artist and I practice design from a feministic point of view. I believe the best businesses and projects are driven by true diversity and transparency. 
+
+<br><br>Thank you for stopping by my little corner of the internet. Let’s connect.
+
+
+
+
+ </p>
+    </div>
  </div>
 </template>
