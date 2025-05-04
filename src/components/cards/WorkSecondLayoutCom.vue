75d48ecd5e4e4141051c7673c6392deb2e15dd05
Helena Hollænder <helena.hollaender@gmail.com>
2025-05-04 16:17:26 +0200

    det_er_nice

--- src/components/cards/WorkSecondLayoutCom.vue
+++ src/components/cards/WorkSecondLayoutCom.vue
@@ -6,8 +6,15 @@
     <div class="col-span-2 col-start-2 h-[20vw] bg-white row-span-2">
         <img src="/images/toys/flogger3dToys.jpg" class="w-full h-full object-cover" />
     </div>
-    <div class="col-span-3 col-start-5 row-start-2 row-span-4 h-[40vw] bg-white">
+    <div class="col-span-3 col-start-5 row-start-2 row-span-5 h-[50vw] bg-white">
         <img src="/images/toys/flogger3dToys.jpg" class="w-full h-full object-cover" />
+    </div>
+    <div class="col-span-2 col-start-9 row-start-6 row-span-2 h-[20vw]">
+        <img src="/images/toys/flogger3dToys.jpg" class="w-full h-full object-cover" />
+
+    </div>
+    <div class="col-span-5 col-start-2 row-start-8 row-span-3 h-[30vw]">
+        <img src="/images/toys/flogger3dToys.jpg" class=" h-full w-[90%] object-cover" />
 
     </div>
 
