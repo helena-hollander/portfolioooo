34c3af592c730dc64777d6053836ceaab74d85d5
Helena Hollænder <helena.hollaender@gmail.com>
2025-05-05 19:16:11 +0200

    scribbles_added_hero

--- src/components/cards/WorkScribbleLayoutCom.vue
+++ src/components/cards/WorkScribbleLayoutCom.vue
@@ -6,8 +6,35 @@
         <div class="col-span-2 col-start-2 h-[20vw] row-start-2 row-span-2">
             <img src="/images/work_tags/heroScribbles/toyScribbleHero.png" class="h-[60%] w-full object-cover"/>
         </div>
-        <div class="col-span-2 col-start-7 h-[40vw] row-start-2 row-span-4 justify-items-end">
-            <img src="/images/work_tags/heroScribbles/lolaScribbleHero.png" class="h-full  object-cover"/>
+        <div class="col-span-2 col-start-7 h-[40vw] row-start-1 row-span-4 justify-items-end content-end">
+            <img src="/images/work_tags/heroScribbles/lolaScribbleHero.png" class="h-[90%]  object-cover"/>
+        </div>
+        <div class="col-span-2 col-start-8 h-[20vw] row-start-5 row-span-2 justify-items-end">
+            <img src="/images/work_tags/heroScribbles/kingScribbleHero.png" class=" w-[70%] mr-[10%] mt-[20%] object-cover"/>
+        </div>
+        <div class="col-span-3 col-start-7 h-[20vw] row-start-8 row-span-2">
+            <img src="/images/work_tags/heroScribbles/tunniitScribbleHero.png" class=" w-[100%] object-cover"/>
+        </div>
+        <div class="col-span-6 col-start-1 h-[20vw] row-start-12 row-span-2 justify-items-end">
+            <img src="/images/work_tags/heroScribbles/genHopeScribbleHero.png" class=" w-[96%] object-cover"/>
+        </div>
+        <div class="col-span-3 col-start-8 h-[20vw] row-start-[17] row-span-2">
+            <img src="/images/work_tags/heroScribbles/threejsScribbleHero.png" class=" w-[96%] object-cover"/>
+        </div>
+        <div class="col-span-6 col-start-1 h-[20vw] row-start-[18] row-span-2 justify-items-end">
+            <img src="/images/work_tags/heroScribbles/circleScribbleHero.png" class=" w-[90%] object-cover"/>
+        </div>
+        <div class="col-span-4 col-start-2 h-[20vw] row-start-[24] row-span-2 justify-items-center">
+            <img src="/images/work_tags/heroScribbles/inbattleScribbleHero.png" class=" w-[84%] object-cover"/>
+        </div>
+        <div class="col-span-2 col-start-9 h-[20vw] row-start-[26] row-span-2 justify-items-center">
+            <img src="/images/work_tags/heroScribbles/stonesScribbleHero.png" class=" w-[68%] object-cover"/>
+        </div>
+        <div class="col-span-3 h-[20vw] row-start-[27] row-span-2 justify-items-center">
+            <img src="/images/work_tags/heroScribbles/rundetaarnScribbleHero.png" class="w-full object-cover mt-[18%]"/>
+        </div>
+        <div class="col-span-7 col-start-4 h-[20vw] row-start-[32] row-span-2 justify-items-center">
+            <img src="/images/work_tags/heroScribbles/portraitScribbleHero.png" class="w-full object-cover mt-[18%]"/>
         </div>
 
 
