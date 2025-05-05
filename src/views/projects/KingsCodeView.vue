f7ef59395b7a2870f6c6fdbb8565616ba9f583b7
Helena Hollænder <helena.hollaender@gmail.com>
2025-05-05 22:11:08 +0200

    king_files

--- src/views/projects/KingsCodeView.vue
+++ src/views/projects/KingsCodeView.vue
@@ -4,4 +4,10 @@ import FooterCom from '@/components/universal/FooterCom.vue';
 </script>
 <template>
     <p>King</p>
+    <div class="cardContainer grid grid-cols-10 grid-rows-[repeat(38,minmax(10vw,auto))]">
+
+        <div class="col-span-2 col-start-2 h-[20vw] bg-white row-span-2">
+            <img/>
+        </div>
+    </div>
 </template>
