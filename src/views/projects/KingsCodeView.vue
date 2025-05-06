11e921d372af040236007ebbb4bbe13ad066e84c
Helena Hollænder <helena.hollaender@gmail.com>
2025-05-06 16:11:40 +0200

    reset_king_for_mobile_layout

--- src/views/projects/KingsCodeView.vue
+++ src/views/projects/KingsCodeView.vue
@@ -4,15 +4,15 @@ import FooterCom from '@/components/universal/FooterCom.vue';
 import ProjectTextCom from '@/components/projects/projectCards/ProjectTextCom.vue';
 </script>
 <template>
-    <div class="absolute grid grid-cols-10 grid-rows-[repeat(30,minmax(10vw,auto))]">
-        <div class="col-span-3 col-start-2 h-[20vw] bg-white row-span-2 row-start-2">
+    <div class="absolute grid grid-cols-1 grid-rows-auto sm:grid-cols-10 grid-rows-[repeat(30,minmax(10vw,auto))]">
+        <div class="col-start-1 col-span-1 row-start-1 sm:col-span-1 sm:row-start-1 sm:col-span-3 sm:col-start-2 sm:h-[20vw]  sm:row-span-2 sm:row-start-2">
             <img src="/images/kingscode/me.jpeg" class="w-full object-cover"/>
         </div>
-        <div class="col-span-5 col-start-5 h-[20vw] row-span-2 row-start-3 justify-items-end">
-            <img src="/images/kingscode/kongePresse.jpeg" class="w-[90%] object-cover"/>
+        <div class="sm:col-span-5 sm:col-start-5 sm:h-[20vw] sm:row-span-2 sm:row-start-3 sm:justify-items-end">
+            <img src="/images/kingscode/kongePresse.jpeg" class="sm:w-[90%] object-cover"/>
         </div>
 
-        <div class="col-span-5 col-start-5 h-[20vw] row-span-2 row-start-2">
+        <div class="sm:col-span-5 sm:col-start-5 sm:h-[20vw] sm:row-span-2 sm:row-start-2">
            
             <ProjectTextCom
                 title="King's visit"
@@ -20,7 +20,7 @@ import ProjectTextCom from '@/components/projects/projectCards/ProjectTextCom.vu
                 textContent="Processing with the King of Denmark"
                  />
         </div>
-        <div class="col-span-4 col-start-1 h-[20vw] row-span-3 row-start-4 justify-items-end m-[4%]">
+        <div class="sm:col-span-4 sm:col-start-1 sm:h-[20vw] sm:row-span-3 sm:row-start-4 sm:justify-items-end sm:m-[4%]">
            
             <ProjectTextCom
                 title="King's visit"
@@ -30,39 +30,39 @@ import ProjectTextCom from '@/components/projects/projectCards/ProjectTextCom.vu
            
             
         </div>
-        <div class="col-span-3 col-start-5 h-[20vw] row-span-2 row-start-6 ml-[20%]">
+        <div class="sm:col-span-3 sm:col-start-5 sm:h-[20vw] sm:row-span-2 sm:row-start-6 sm:ml-[20%]">
             <ProjectTextCom
                 title="Press image"
                 textType="description"
                 textContent="// Photo by Christian Falch Wolff"
                  />
         </div>
-        <div class="col-span-5 col-start-6 h-[20vw] row-span-3 row-start-7 m-[4%]">
+         <div class="sm:col-span-5 sm:col-start-6 sm:h-[20vw] sm:row-span-3 sm:row-start-7 sm:m-[4%]">
             <ProjectTextCom
                 title="King's visit"
                 textType="body"
                 textContent="The sketch was made in Processing, and displayed a pixel-like grid, with sqaures that changed color, mapped from the camera input, and animated on sound. The colors were picked from random vectors on images of the King himself. We could scroll through an array of palettes from keyboard inputs, and the size of the squares was mapped to the x-position of the mouse."
                  />
         </div>
-        <div class="col-span-6 col-start-3 h-[20vw] row-span-2 row-start-11 justify-items-end">
-            <video class=" w-full object-cover" autoplay loop muted playsinline>
+        <div class="sm:col-span-6 sm:col-start-3 sm:h-[20vw] sm:row-span-2 sm:row-start-11 sm:justify-items-end">
+            <video class=" sm:w-full object-cover" autoplay loop muted playsinline>
                 <source src="/videos/kingscode/kingScreenRecording.mp4" type="video/mp4">
             </video>
         </div>
-        <div class="col-span-5 col-start-1 h-[20vw] row-span-2 row-start-8 justify-items-end">
-            <img src="/images/kingscode/hanniPeace.jpeg" class="w-[80%] object-cover"/>
+       <div class="sm:col-span-5 sm:col-start-1 sm:h-[20vw] sm:row-span-2 sm:row-start-8 sm:justify-items-end">
+            <img src="/images/kingscode/hanniPeace.jpeg" class="sm:w-[80%] object-cover"/>
         </div>
-        <div class="col-span-4 col-start-7 h-[20vw] row-span-2 row-start-[15] justify-items-center">
-            <img src="/images/kingscode/kongePresse2.jpeg" class="w-[90%] object-cover"/>
+        <div class="sm:col-span-4 sm:col-start-7 sm:h-[20vw] sm:row-span-2 sm:row-start-[15] sm:justify-items-center">
+            <img src="/images/kingscode/kongePresse2.jpeg" class="sm:w-[90%] object-cover"/>
         </div>
-        <div class="col-span-3 col-start-3 h-[10vw] row-span-1 row-start-[14] ml-[2%] content-center">
+        <div class="sm:col-span-3 sm:col-start-3 sm:h-[10vw] sm:row-span-1 sm:row-start-[14] sm:ml-[2%] sm:content-center">
             <ProjectTextCom
                 title="Sketch recording"
                 textType="description"
                 textContent="// Screenrecording of Processing sketch"
                  />
         </div>
-        <div class="col-span-3 col-start-7 h-[10vw] row-span-1 row-start-[17] ml-[10%] content-center">
+        <div class="sm:col-span-3 sm:col-start-7 sm:h-[10vw] sm:row-span-1 sm:row-start-[17] sm:ml-[10%] sm:content-center">
             <ProjectTextCom
                 title="Press image"
                 textType="description"
@@ -70,36 +70,36 @@ import ProjectTextCom from '@/components/projects/projectCards/ProjectTextCom.vu
                  />
         </div>
 
-        <div class="col-span-3 col-start-2 h-[30vw] row-span-3 row-start-[17] content-center">
-            <img src="/images/kingscode/processingTiles.jpeg" class="w-[80%] object-cover"/>
+        <div class="sm:col-span-3 sm:col-start-2 sm:h-[30vw] sm:row-span-3 sm:row-start-[17] sm:content-center">
+            <img src="/images/kingscode/processingTiles.jpeg" class="sm:w-[80%] object-cover"/>
         </div>
-        <div class="col-span-4 col-start-5 h-[30vw] row-span-3 row-start-[18] content-center justify-items-center">
-            <img src="/images/kingscode/colorPicker.jpeg" class="w-[80%] object-cover"/>
+        <div class="sm:col-span-4 sm:col-start-5 sm:h-[30vw] sm:row-span-3 sm:row-start-[18] sm:content-center sm:justify-items-center">
+            <img src="/images/kingscode/colorPicker.jpeg" class="sm:w-[80%] object-cover"/>
         </div>
-        <div class="col-span-4 col-start-5 h-[30vw] row-span-3 row-start-[21] ">
+        <div class="sm:col-span-4 sm:col-start-5 sm:h-[30vw] sm:row-span-3 sm:row-start-[21] ">
             <ProjectTextCom
                 title="Press image"
                 textType="description"
                 textContent="// Random color picker on an image from Royal Run"
-                class="mt-[-16%] ml-[10%]"
+                class="sm:mt-[-16%] sm:ml-[10%]"
                  />
         </div>
-        <div class="col-span-4 col-start-7 h-[30vw] row-span-3 row-start-[21] content-center">
-            <img src="/images/kingscode/kingPixels3.jpeg" class="w-[90%] object-cover"/>
+        <div class="sm:col-span-4 sm:col-start-7 sm:h-[30vw] sm:row-span-3 sm:row-start-[21] sm:content-center">
+            <img src="/images/kingscode/kingPixels3.jpeg" class="sm:w-[90%] object-cover"/>
         </div>
-        <div class="col-span-4 col-start-3 h-[30vw] row-span-3 row-start-[21] content-center">
-            <img src="/images/kingscode/kingPixels1.jpeg" class="w-[90%] object-cover"/>
+        <div class="sm:col-span-4 sm:col-start-3 sm:h-[30vw] sm:row-span-3 sm:row-start-[21] sm:content-center">
+            <img src="/images/kingscode/kingPixels1.jpeg" class="sm:w-[90%] object-cover"/>
         </div>
-        <div class="col-span-3 col-start-1 h-[30vw] row-span-3 row-start-[23] content-center">
-            <img src="/images/kingscode/processingPallette.jpeg" class="w-[90%] object-cover"/>
+        <div class="sm:col-span-3 sm:col-start-1 sm:h-[30vw] sm:row-span-3 sm:row-start-[23] sm:content-center">
+            <img src="/images/kingscode/processingPallette.jpeg" class="sm:w-[90%] object-cover"/>
         </div>
-        <div class="col-span-5 col-start-4 h-[30vw] row-span-3 row-start-[26] justify-items-end">
-            <img src="/images/kingscode/stigAndMe.jpeg" class="w-[90%] object-cover"/>
+       <div class="sm:col-span-5 sm:col-start-4 sm:h-[30vw] sm:row-span-3 sm:row-start-[26] sm:justify-items-end">
+            <img src="/images/kingscode/stigAndMe.jpeg" class="sm:w-[90%] object-cover"/>
         </div>
-        <div class="col-span-3 col-start-1 h-[30vw] row-span-3 row-start-[28] justify-items-end ">
-            <img src="/images/kingscode/hanniShout.jpeg" class="w-[90%] object-cover mt-[2%]"/>
+        <div class="sm:col-span-3 sm:col-start-1 sm:h-[30vw] sm:row-span-3 sm:row-start-[28] sm:justify-items-end ">
+            <img src="/images/kingscode/hanniShout.jpeg" class="sm:w-[90%] object-cover sm:mt-[2%]"/>
         </div>
-        <div class="col-span-6 col-start-4 h-[20vw] row-span-2 row-start-[24] ">
+        <div class="sm:col-span-6 sm:col-start-4 sm:h-[20vw] sm:row-span-2 sm:row-start-[24] ">
             <ProjectTextCom
                 title="Press image"
                 textType="body"
@@ -112,21 +112,27 @@ import ProjectTextCom from '@/components/projects/projectCards/ProjectTextCom.vu
                 textContent="Thank you Stig, for including me in this little adventure. I think we nailed it!"
                 class=""
                  />
+            <ProjectTextCom
+                title="Press image"
+                textType="description"
+                textContent="// March 2024"
+                class=""
+                 />
         </div>
-        <div class="col-span-5 col-start-4 h-[10vw] row-span-1 row-start-[29] justify-items-start">
+        <div class="sm:col-span-5 sm:col-start-4 sm:h-[10vw] sm:row-span-1 sm:row-start-[29] sm:justify-items-start">
             <ProjectTextCom
                 title="Press image"
                 textType="description"
                 textContent="// Stig and I making weird faces to the camera input"
-                class=" w-full ml-[10%] mt-[-4%]"
+                class=" sm:w-full sm:ml-[10%] sm:mt-[-4%]"
                  />
         </div>
-        <div class="col-span-4 col-start-1 h-[10vw] row-span-1 row-start-[30] ">
+        <div class="sm:col-span-4 sm:col-start-1 sm:h-[10vw] sm:row-span-1 sm:row-start-[30] ">
             <ProjectTextCom
                 title="Press image"
                 textType="description"
                 textContent="// Hannibal and I shouting to test the mic input"
-                class="mt-[-7%] ml-[8%]"
+                class="sm:mt-[-7%] sm:ml-[8%]"
                  />
         </div>
     </div>
