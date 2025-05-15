352631cac39ce7d9acb3fb08576d874439154a6c
Helena Hollænder <helena.hollaender@gmail.com>
2025-05-15 20:17:58 +0200

    dsr_start

--- src/views/projects/DsrView.vue
+++ src/views/projects/DsrView.vue
@@ -1,7 +1,13 @@
 <script setup>
 import { ref } from 'vue'
 import FooterCom from '@/components/universal/FooterCom.vue';
+import ProjectTextCom from '@/components/projects/projectCards/ProjectTextCom.vue';
+
 </script>
 <template>
-    <p>DSR</p>
+   <div class="absolute grid grid-cols-1 grid-rows-auto gap-[0.5%] sm:grid-cols-10 sm:grid-rows-[repeat(30,minmax(10vw,auto))] sm:gap-0">
+        <div class="col-start-1 col-span-1 row-start-1 justify-items-center sm:justify-items-start  sm:row-start-1 sm:col-span-3 sm:col-start-2 sm:h-[20vw]  sm:row-span-2 sm:row-start-2">
+            <img src="/images/inbattle/inbattleSketch1.png" class="w-[0%] sm:w-full object-cover"/>
+        </div>
+    </div>
 </template>
