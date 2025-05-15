526ee36b3a3885391b4600c4d0f2e762a99e0fea
Helena Hollænder <helena.hollaender@gmail.com>
2025-05-15 22:11:52 +0200

    desktop_dsr_done

--- src/views/projects/DsrView.vue
+++ src/views/projects/DsrView.vue
@@ -5,7 +5,7 @@ import ProjectTextCom from '@/components/projects/projectCards/ProjectTextCom.vu
 
 </script>
 <template>
-   <div class="absolute grid grid-cols-1 grid-rows-auto gap-[0.5%] sm:grid-cols-10 sm:grid-rows-[repeat(30,minmax(10vw,auto))] sm:gap-0">
+   <div class="absolute grid grid-cols-1 grid-rows-auto gap-[0.5%] sm:grid-cols-10 sm:grid-rows-[repeat(28,minmax(10vw,auto))] sm:gap-0">
         <div class="col-start-1 col-span-1 row-start-1 justify-items-center sm:justify-items-start  sm:row-start-1 sm:col-span-3 sm:col-start-2 sm:h-[20vw]  sm:row-span-2 sm:row-start-2">
             <img src="/images/dsr/dsrSketch1.png" class="w-[0%] sm:w-full object-cover"/>
         </div>
@@ -66,8 +66,17 @@ import ProjectTextCom from '@/components/projects/projectCards/ProjectTextCom.vu
         <div class="justify-items-center sm:justify-items-cente sm:col-span-2 sm:col-start-4 sm:h-[20vw] sm:row-span-2 sm:row-start-[23]">
             <img src="/images/dsr/poster1.jpeg" class="w-[0%] sm:w-[80%] object-cover"/>
         </div>
+        <div class="justify-items-center sm:justify-items-cente sm:col-span-3 sm:col-start-8 sm:h-[20vw] sm:row-span-2 sm:row-start-[24]">
+            <img src="/images/dsr/stella.jpeg" class="w-[0%] sm:w-[80%] object-cover"/>
+        </div>
+        <div class="justify-items-center sm:justify-items-cente sm:col-span-5 sm:col-start-4 sm:h-[20vw] sm:row-span-2 sm:row-start-[25]">
+            <img src="/images/dsr/Regnbue(hvid).png" class="w-[0%] sm:w-[80%] object-cover"/>
+        </div>
+        <div class="justify-items-center sm:justify-items-cente sm:col-span-3 sm:col-start-1 sm:h-[20vw] sm:row-span-2 sm:row-start-[26]">
+            <img src="/images/dsr/På-Hoved(hvid).png" class="w-[0%] sm:w-[76%] object-cover"/>
+        </div>
 
-        <div class="row-start-3 sm:col-span-4 sm:col-start-1 sm:h-[20vw] sm:row-span-2 sm:row-start-5">
+        <div class=" sm:col-span-4 sm:col-start-1 sm:h-[20vw] sm:row-span-2 sm:row-start-5">
            
            <ProjectTextCom
                title="Title"
@@ -95,13 +104,53 @@ import ProjectTextCom from '@/components/projects/projectCards/ProjectTextCom.vu
                 />
           
        </div>
-        <div class="row-start-3 sm:col-span-4 sm:col-start-3 sm:h-[20vw] sm:row-span-2 sm:row-start-[17]">
+        <div class=" sm:col-span-4 sm:col-start-3 sm:h-[20vw] sm:row-span-2 sm:row-start-[17]">
            <ProjectTextCom
                title="Intro"
                textType="body"
                textContent="We decided a combination of brirght, playfull colors, a funky logo-type and I created a series of character illustrations. They all represent the everyday life, ups and downs of being a student at DMJX. They are purposefully made in a gender-neutral likeness, to be diverse and relateable across genders and backrounds."
-               class="w-[280px] mx-[4%] sm:w-[500px]"
+               class="w-[280px] mx-[4%] sm:w-full"
                 />
+          
+       </div>
+       <div class=" sm:col-span-2 sm:col-start-2 sm:h-[10vw] sm:row-span-1 sm:row-start-[14]">
+           <ProjectTextCom
+           title="Intro"
+           textType="description"
+           textContent="// Stickers"
+           class="w-[280px] sm:mx-[2%] sm:w-full sm:mt-[19%]"
+           />
+        </div>
+        <div class=" sm:col-span-2 sm:col-start-8 sm:h-[20vw] sm:row-span-2 sm:row-start-[19]">
+            <ProjectTextCom
+            title="Intro"
+            textType="description"
+            textContent="// Illustrations done in Procreate"
+            class="w-[280px] sm:mx-[6%] sm:w-full sm:mt-[0%]"
+            />
+        </div>
+        <div class=" sm:col-span-2 sm:col-start-8 sm:h-[20vw] sm:row-span-2 sm:row-start-[26]">
+            <ProjectTextCom
+            title="Intro"
+            textType="description"
+            textContent="// This is Stella <3"
+            class="w-[280px] sm:mx-[18%] sm:w-full sm:mt-[-10%]"
+            />
+            
+        </div>
+        <div class=" sm:col-span-4 sm:col-start-1 sm:h-[20vw] sm:row-span-2 sm:row-start-[25]">
+           <ProjectTextCom
+               title="Intro"
+               textType="body"
+               textContent="Take a walk around the school and see the posters, stickers and illustrations bringing color and community. Spreading important messages and adorning our social events."
+               class="w-[280px] mx-[4%] sm:w-full"
+                />
+          
+       </div>
+        <div class=" sm:col-span-2 sm:col-start-1 sm:h-[20vw] sm:row-span-2 sm:row-start-[24]">
+           <p  class="ml-[10%] sm:mt-[18%] text-darkest font-vazirmatn text-descriptionSizeMobile sm:text-descriptionSize">
+            //Link to <a href="https://www.instagram.com/dsr.kbh/" class="hover:underline hover:text-lightContrast" target="_blank">DSR's Instragram</a>
+             </p>
           
        </div>
     </div>
